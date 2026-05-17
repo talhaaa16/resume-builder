4248a649c7fd21380c90c7495a190b159abfb336
talhaaa16 <talhashaikh200218@gmail.com>
2026-05-17 14:59:11 +0530

    feat: add more custom typepicker while creating resumes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import axios from "axios";
+import "./index.css";
 
 axios.interceptors.response.use(
   (response) => response,
